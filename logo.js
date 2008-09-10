93d1edb47762e4ae8df6ab52566e602cafc986c9
Thomas Figg <tef@twentygototen.org>
2008-09-10 22:46:11 +0000

    minor tweaks, added a tokenizer
    git-svn-id: http://papert.googlecode.com/svn/trunk@3 e7052fda-7f78-11dd-a143-0dd59a09f4aa

--- logo.js
+++ logo.js
@@ -11,32 +11,79 @@ function eval_logo(code, turtle) {
 function logo_to_js_list(code, checkend) {
     var js = new Array();
    
-    js.push("function (t) {");
-   
-    code=code.split(/\n/).join(";");
-    
-    var instr = /^\s*(\w+)\s+(\d+)\s*;\s*(.*)/i;
-    var insr = /^\s*(\w+)\s*;\s*(.*)/i;
-    
-    
-    var loop = true;
-    
-    while (loop) {
-        if ((result = instr.exec(code)) != null) {
-            js.push("t."+result[1].toLowerCase()+"("+result[2]+");");
-            loop=true;
-            code= result[3]; 
-        } else if ((result = insr.exec(code)) != null) {
-            js.push("t."+result[1].toLowerCase()+"();");
-            loop=true;
-            code= result[2]; 
-        } else {
-            loop=false;
-        }
-    }
-    
-    js.push("}");
-    
+    var p = parse(code); 
     return  js;
 }
 
+function parse(text) {
+    
+    var tk = new Tokenizer(text);
+
+    var norm = new Array();
+        norm['pu'] = 'penup';
+        norm['pd'] = 'pendown';
+        norm['fw'] = 'forward';
+        norm['bw'] = 'backward';
+        norm['cs'] = 'clear';
+        norm['rt'] = 'right';
+        norm['lt'] = 'left';
+
+    do {
+        token = tk.next()
+        if (token == null)  break;
+        if (token[0] == 'error') {
+            alert("Error: unexpected text: "+token[1]);
+            return null;
+        } else if (token[0] == 'id') {
+            if (norm[token[1]] != null) {
+                token[1] = norm[token[1]]
+            }
+        }
+        alert(token);
+    } while(true);
+}
+
+function Tokenizer(text) {
+
+    this.text = text;
+
+    this.id_rx = /^\s*([a-zA-Z]\w*)\s*/i;
+    this.var_rx = /^\s*(:[a-zA-Z]\w*)\s*/i;
+    this.num_rx = /^\s*(\d+(?:\.\d+)?)\s*/i;
+    this.str_rx = /^\s*"((?:(?!\\)[^"]|\\")*)"\s*/i;
+
+    this.empty = /^\s*$/;
+    this.next = function () {
+
+        if (this.empty.exec(this.text)) {
+            this.text = null;
+            return null;
+
+        } else if ((result = this.id_rx.exec(this.text)) != null) {
+            this.text = this.text.substring(result[0].length)
+            return ['id',result[1]];
+
+        } else if ((result = this.var_rx.exec(this.text)) != null) {
+            this.text = this.text.substring(result[0].length)
+            return ['var',result[1]];
+
+        } else if ((result = this.num_rx.exec(this.text)) != null) {
+            this.text = this.text.substring(result[0].length)
+            return ['num',result[1]];
+
+        } else if ((result = this.str_rx.exec(this.text)) != null) {
+            this.text = this.text.substring(result[0].length)
+            return ['str',result[1]];
+
+       /* } else if ((result = this.regex.exec(this.text)) != null) {
+            this.text = this.text.substring(result[0].length)
+            return ['id',result[1]];
+       */
+        } else {
+            return ['error', this.text];
+        }
+    }   
+}
+
+
+            
--- turtle.js
+++ turtle.js
@@ -8,18 +8,13 @@ function Turtle (canvas) {
         this.max_y = canvas.height;
         
         this.x = canvas.width/2;
-        this.y = canvas.height/2;
-        
-        this.angle = 270;
-        
-        this.pen = true;
 
-        this.c.lineCap = "round"
+        this.c.lineCap = "round";
         
 
         this.jump = function(x,y) {
-            this.x = x
-            this.y = y
+            this.x = x;
+            this.y = y;
         }
         
         this.penwidth = function(w) {
@@ -50,52 +45,66 @@ function Turtle (canvas) {
             this.y = newy;
         }
         
-        this.fw = this.forward
+        this.fw = this.forward;
        
         this.backward = function (d) {
             this.forward(-d);
         }
         
-        this.bk = this.backward
+        this.bk = this.backward;
         
         this.right = function(angle) {
             this.angle = (this.angle + angle) % 360;
         }
         
-        this.rt = this.right
+        this.rt = this.right;
         
         this.left = function(angle) {
             this.right(-angle);
         }
 
-        this.lt = this.left
+        this.lt = this.left;
         
         this.penup = function() {
             this.pen = false;
         }
 
+        this.pd = this.penup;
+
         this.pendown = function() {
             this.pen = true;
         }
 
+	    this.pd = this.pendown;
+
         this.radians = function() {
             return this.angle / 180 * Math.PI;
-        } 
+        }
+ 
         this.clear = function() {
             old = this.c.fillStyle
             this.c.fillStyle = "rgb(255,255,255)";
             this.c.fillRect(0,0,this.max_x,this.max_y);
             this.c.fillStyle = old
         }
-        
+
+        this.cs = this.clear;
+
         this.reset = function() {
             this.clear();
+            this.setup();
+        }
+
+        this.setup = function() {
             this.x = this.max_x/2;
             this.y = this.max_y/2;
             this.angle = 270;
             this.penwidth(1);
             this.color("rgb(0,0,0)");
+            this.pen = true;
         }
+
+        this.setup();
             
     }
 }
