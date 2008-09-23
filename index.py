f8d6e2d35c628dd33addf60dec8472cce2837037
Thomas Figg <tef@twentygototen.org>
2008-09-23 14:29:09 +0000

    adding image previews.
    git-svn-id: http://papert.googlecode.com/svn/trunk@89 e7052fda-7f78-11dd-a143-0dd59a09f4aa

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 """$Id$"""
 import os
+import base64
 
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from google.appengine.api import images
 
 class LogoProgram(db.Model):
     code = db.TextProperty()
+    img  = db.BlobProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
 
@@ -16,35 +19,50 @@ class Papert(webapp.RequestHandler):
       
     key = self.request.path.split('/',1)[1]
     
+    if key.find('/') >=0:
+        key, args = key.split('/',1)
+    else:
+        args = None
+    
     page = os.path.join(os.path.dirname(__file__), 'index.html.tmpl')
     
     values = {'code':""}
+
+    program = None
     
     if key:
         try:
             program = db.get(key)
-            if program:
-                values['code'] = program.code
         except db.BadKeyError:
             pass
 
-        
-    self.response.out.write(template.render(page, values))
+    if program and args == "img":
+        self.response.headers['Content-Type'] = 'image/png'
+        self.response.out.write(program.img)
+    else:
+        if program:
+            values['code'] = program.code
+            values['key'] = key
+            
+        self.response.out.write(template.render(page, values))
 
   def post(self):
     
     code = self.request.get('code',None)
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write("cocks\n")
-
-
+    img = self.request.get('img',"")
     if code:
         program = LogoProgram()
         program.code = code
-        program.hash = hash
+        if img:
+            img=base64.b64decode(img)
+            img = images.Image(img)
+            img.resize(width = 125, height = 125)
+            program.img = img.execute_transforms(output_encoding=images.PNG)
+            
         key = program.put()
     else:
         key = ""
+    
     self.redirect("/%s"%key)
     
 application = webapp.WSGIApplication([('/.*', Papert)],debug=True)
