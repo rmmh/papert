8a5a881e4a70e890f2567690272ffcb587f87df3
Ryan Hitchman <hitchmanr@gmail.com>
2008-09-24 00:31:08 +0000

    shortening links, improving css, fixing html
    git-svn-id: http://papert.googlecode.com/svn/trunk@94 e7052fda-7f78-11dd-a143-0dd59a09f4aa

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 """$Id$"""
 import os
 import base64
+import sha
+import re
 
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
@@ -12,58 +14,51 @@ class LogoProgram(db.Model):
     code = db.TextProperty()
     img  = db.BlobProperty()
     date = db.DateTimeProperty(auto_now_add=True)
+    hash = db.StringProperty()
 
+hash_re = re.compile(r"/([A-Za-z0-9_-]*)(.*)")
 
 class Papert(webapp.RequestHandler):
   def get(self):
-      
-    key = self.request.path.split('/',1)[1]
-    
-    if key.find('/') >=0:
-        key, args = key.split('/',1)
-    else:
-        args = None
-    
-    page = os.path.join(os.path.dirname(__file__), 'index.html.tmpl')
+    hash, extra = hash_re.match(self.request.path).groups()
     
     values = {'code':""}
 
     program = None
     
-    if key:
-        try:
-            program = db.get(key)
-        except db.BadKeyError:
-            pass
+    if hash:
+        program = LogoProgram.all().filter('hash = ', hash).get()
 
-    if program and args == "img":
+    if program and extra == ".png":
         self.response.headers['Content-Type'] = 'image/png'
         self.response.out.write(program.img)
     else:
         if program:
             values['code'] = program.code
-            values['key'] = key
-            
+            values['hash'] = hash
+    
+        page = os.path.join(os.path.dirname(__file__), 'index.html.tmpl')
         self.response.out.write(template.render(page, values))
 
   def post(self):
-    
     code = self.request.get('code',None)
     img = self.request.get('img',"")
-    if code:
-        program = LogoProgram()
-        program.code = code
-        if img:
-            img=base64.b64decode(img)
-            img = images.Image(img)
-            img.resize(width = 125, height = 125)
-            program.img = img.execute_transforms(output_encoding=images.PNG)
-            
-        key = program.put()
+    if code.strip():
+        hash = base64.b64encode(sha.sha(code.strip()).digest()[:6], "-_")
+        if not LogoProgram.all().filter('hash =', hash).get():
+            program = LogoProgram()
+            program.code = code
+            program.hash = hash
+            if img:
+                img = base64.b64decode(img)
+                img = images.Image(img)
+                img.resize(width = 125, height = 125)
+                program.img = img.execute_transforms(output_encoding=images.PNG)
+            program.put()
     else:
-        key = ""
+        hash = ""
     
-    self.redirect("/%s"%key)
+    self.redirect("/%s" % hash)
     
 application = webapp.WSGIApplication([('/.*', Papert)],debug=True)
 
