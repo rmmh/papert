f8757e1db1edac0ce32074351b141ece3269ff0b
Ryan Hitchman <hitchmanr@gmail.com>
2008-09-25 22:35:16 +0000

    making indentation more consistent
    git-svn-id: http://papert.googlecode.com/svn/trunk@109 e7052fda-7f78-11dd-a143-0dd59a09f4aa

--- index.py
+++ index.py
@@ -17,84 +17,85 @@ class LogoProgram(db.Model):
     hash = db.StringProperty()
 
 class Papert(webapp.RequestHandler):
-  def get(self):
-    hash = self.request.path[1:9] #this assumes that hashes are always 8 chars
-    extra = self.request.path[9:]
+    def get(self):
+        hash = self.request.path[1:9] #this assumes that hashes are always 8 chars
+        extra = self.request.path[9:]
 
-    if extra == ".png" and hash == self.request.headers.get('If-None-Match'):
-        self.response.set_status(304)
-        return
+        if extra == ".png" and hash == self.request.headers.get('If-None-Match'):
+            self.response.set_status(304)
+            return
 
-    if extra not in ('', '.png'):
-        self.redirect('/')
-        return
-
-    program = None
-
-    if hash:
-        program = memcache.get("program: %s" % hash)
-        if program is None:
-            program = LogoProgram.all().filter('hash = ', hash).get()
-            if program is None:
-                memcache.set("program: %s" % hash, "not found")
-            else:
-                memcache.set("program: %s" % hash, program)
-
-        if program == "not found":
-            program = None
-
-        if program is None:
+        if extra not in ('', '.png'):
             self.redirect('/')
+            return
 
-    if program and extra == ".png":
-        self.response.headers['Content-Type'] = 'image/png'
-        self.response.headers['Cache-Control'] = 'max-age:604800'
-        self.response.headers['Etag'] = program.hash
-        self.response.headers['Last-Modified'] = program.date.ctime()
-        self.response.out.write(program.img)
-    else:
-        values = {'code':""}
-        if program:
-            values['code'] = program.code
-            values['hash'] = hash
+        program = None
+
+        if hash:
+            program = memcache.get("program: %s" % hash)
+            if program is None:
+                program = LogoProgram.all().filter('hash = ', hash).get()
+                if program is None:
+                    memcache.set("program: %s" % hash, "not found")
+                else:
+                    memcache.set("program: %s" % hash, program)
+
+            if program == "not found":
+                program = None
+
+            if program is None:
+                self.redirect('/')
+
+        if program and extra == ".png":
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.headers['Cache-Control'] = 'max-age:604800'
+            self.response.headers['Etag'] = program.hash
+            self.response.headers['Last-Modified'] = program.date.ctime()
+            self.response.out.write(program.img)
+        else:
+            values = {'code':""}
+            if program:
+                values['code'] = program.code
+                values['hash'] = hash
         
-        recent = memcache.get("recent")
-        if recent is None:
-            recent = [program.hash for program in
-                      LogoProgram.all().order('-date').fetch(5)]
-            memcache.set("recent", recent)
+            recent = memcache.get("recent")
+            if recent is None:
+                recent = [program.hash for program in
+                          LogoProgram.all().order('-date').fetch(5)]
+                memcache.set("recent", recent)
 
-        values['recent'] = recent
+            values['recent'] = recent
 
-        page = os.path.join(os.path.dirname(__file__), 'index.html.tmpl')
-        self.response.out.write(template.render(page, values))
+            page = os.path.join(os.path.dirname(__file__), 'index.html.tmpl')
+            self.response.out.write(template.render(page, values))
 
-  def post(self):
-    code = self.request.get('code',None)
-    img = self.request.get('img',"")
-    if code.strip():
-        hash = base64.b64encode(sha.sha(code.strip()).digest()[:6], "-_")
-        if not LogoProgram.all().filter('hash =', hash).get():
-            program = LogoProgram()
-            program.code = code
-            program.hash = hash
-            if img:
-                img = base64.b64decode(img)
-                img = images.Image(img)
-                img.resize(125, 125)
-                program.img = img.execute_transforms()
-            program.put()
-            memcache.set("program: %s" % hash, program)
-            memcache.delete("recent")
-    else:
-        hash = ""
+    def post(self):
+        code = self.request.get('code',None)
+        img = self.request.get('img',"")
+
+        if code.strip():
+            hash = base64.b64encode(sha.sha(code.strip()).digest()[:6], "-_")
+            if not LogoProgram.all().filter('hash =', hash).get():
+                program = LogoProgram()
+                program.code = code
+                program.hash = hash
+                if img:
+                    img = base64.b64decode(img)
+                    img = images.Image(img)
+                    img.resize(125, 125)
+                    program.img = img.execute_transforms()
+                program.put()
+                memcache.set("program: %s" % hash, program)
+                memcache.delete("recent")
+        else:
+            hash = ""
     
-    self.redirect("/%s" % hash)
+        self.redirect("/%s" % hash)
     
 application = webapp.WSGIApplication([('/.*', Papert)],debug=True)
 
 def main():
-  run_wsgi_app(application)
+    run_wsgi_app(application)
 
 if __name__ == "__main__":
-  main()
+    main()
