30c813822ca887cdc735d50e55125cb4c48c6601
jywarren <jeff@unterbahn.com>
2015-08-23 18:30:15 -0400

    added SVG export, for use in lasercutters and plotters and vector art

--- static/init.js
+++ static/init.js
@@ -32,17 +32,30 @@ function init(canvas_id, turtle_id, form_id, oldcode_id, textoutput_id) {
     canvas.height = 500;
     
     oldcode = document.getElementById(oldcode_id);
-    setup();
 }
 
-function run(speed, drawbits) {
+function run(speed, drawbits, options) {
+
+    if (options && options.svg) {
+      var _svg = true;
+      document.getElementById('save-svg').setAttribute('style','')
+      document.getElementById('canvas').setAttribute('style','display:none;')
+    } else {
+      var _svg = false;
+      document.getElementById('save-svg').setAttribute('style','display:none;')
+      if (document.getElementById('svg-container') && document.getElementById('svg-container').childNodes.length > 0) document.getElementById('svg-container').childNodes[0].remove();
+      document.getElementById('canvas').setAttribute('style','')
+    }
+
+    setup();
+
     turtle.stop();
     if (speed !== fast) {
         fast = speed;
         var newturtle = null;
         // newturtle = new Turtle(canvas);
         
-        newturtle = new DelayTurtle(canvas, sprite, fast, drawbits);
+        newturtle = new DelayTurtle(canvas, sprite, _svg, fast, drawbits);
         logo.setTurtle(newturtle);
         turtle = newturtle;
     }
@@ -51,6 +64,8 @@ function run(speed, drawbits) {
     //form.code.value = ""
   
     out = logo.run(form.code.value);
+
+    if (_svg) document.getElementById('svg-container').appendChild(turtle.turtle.c.getSvg());
             
     if (out && out.type === "error") {
         alert(out.data);
@@ -60,6 +75,13 @@ function run(speed, drawbits) {
 
 function stop() {
     turtle.stop();
+}
+
+function saveSVG(element) {
+
+    element.setAttribute('download',"papert.svg");
+    element.setAttribute('href',"data:image/svg+xml;utf8," + turtle.getSVGString());
+
 }
 
 function clearcanvas() {
